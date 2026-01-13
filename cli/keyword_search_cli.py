9d082d97aafcdc8618bb7d689d8e1e8c07ea958b
Omachonu Prosper <omachonucodes@gmail.com>
2026-01-13 18:50:06 +0100

    Solution - Tokenization

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -2,47 +2,20 @@
 
 import argparse
 import json
+import string
 
 
-# def preprocess_query(query: str) -> str:
-#     # Case Insensitive 
-#     query = query.lower()
+def preprocess_text(text: str) -> list[str]:
+    # Convert text to lowercase
+    text = text.lower()
 
-#     # Remove punctuation
-#     query = ''.join(char for char in query if char.isalnum() or char.isspace())
+    # Remove punctuation
+    text = text.translate(str.maketrans("", "", string.punctuation))
 
-#     # Tokenize
-#     tokens = query.split()
-
-#     # Remove stop words
-#     stop_words = set({
-#         "i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your",
-#         "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she",
-#         "her", "hers", "herself", "it", "its", "itself", "they", "them", "their",
-#         "theirs", "themselves", "what", "which", "who", "whom", "this", "that",
-#         "these", "those", "am", "is", "are", "was", "were", "be", "been", "being",
-#         "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an",
-#         "the", "and", "but", "if", "or", "because", "as", "until", "while",
-#         "of", "at", "by", "for", "with", "about", "against", "between",
-#         "into", "through",  "during","before","after","above","below","to",
-#         "from","up","down","in","out","on","off","over","under","again",
-#         "further","then","once","here","there","when","where","why","how",
-#         "all","any","both","each","few","more","most","other","some",
-#         "such","no","nor","not","only","own","same","so","than",
-#         "too","very","can","will","just",})
-#     tokens = [word for word in tokens if word not in stop_words]
-
-#     # Stemming (simple suffix stripping)
-#     suffixes = ("ning", "ly", "ed", "ious", "ies", "ive", "es", "s", "ment", "ing")
-#     def stem(word: str) ->  str:
-#         for suffix in suffixes:
-#             if word.endswith(suffix):
-#                 return word[:-len(suffix)]
-#         return word
-
-#     tokens = [stem(word) for word in tokens]
-
-#     return tokens
+    # Tokenize text
+    tokens = text.split()
+    
+    return tokens
 
 
 def search(query: str) -> None:
@@ -50,9 +23,15 @@ def search(query: str) -> None:
     with open('/home/bknd-bobby/projects/rag-search-engine/data/movies.json') as file:
         movies = json.load(file).get("movies", [])
 
-    # tokens = preprocess_query(query)
-    results = [movie for movie in movies if query.lower() in movie["title"].lower()][:5]
-    results.sort(key=lambda x: x['id'])
+    results = []
+    for movie in movies:
+        query_tokens = preprocess_text(query)
+        title_tokens = preprocess_text(movie['title'])
+
+        if any(token in title_token for title_token  in title_tokens for token in query_tokens):
+            results.append(movie)
+    
+    results = results[:5]
     for index, movie in enumerate(results, start=1):
         print(f"{index}. {movie['title']}")
     
